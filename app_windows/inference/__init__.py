4e960389970025f9ea3a4290b2c9068ba332cbfd
bl4z3tt0n3 <149252803+bl4z3tt0n3@users.noreply.github.com>
2026-09-02 00:16:51 +0300

    feat(windows): export fleet face recognition controller

--- app_windows/inference/__init__.py
+++ app_windows/inference/__init__.py
@@ -5,10 +5,12 @@ from .person_detection_controller import (
     PersonDetectionController,
 )
 from .face_recognition_controller import FaceRecognitionController
+from .face_recognition_fleet_controller import FleetFaceRecognitionController
 from .person_detection_fleet_controller import FleetPersonDetectionController
 
 __all__ = [
     "FaceRecognitionController",
+    "FleetFaceRecognitionController",
     "FleetPersonDetectionController",
     "InferenceFrameSource",
     "PersonDetectionController",
